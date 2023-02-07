9361f86bca3101eaac19f2903ef121b34cedc168
Mahmoud THIAM <dooro.mah@gmail.com>
2023-02-07 10:49:56 +0000

    Gestion des minuscules, majuscules et chiffres

--- Exercice 7.py
+++ Exercice 7.py
@@ -1,3 +1,4 @@
+# On considère le clavier
 clavier = {2:["A","B","C"], 
            3:["D","E","F"],
            4:["G","H","I"],
@@ -6,35 +7,48 @@ clavier = {2:["A","B","C"],
            7:["P","Q","R","S"],
            8:["T","U","V"],
            9:["W","X","Y","Z"]}
+keybord = {2:["a","b","c"], 
+           3:["d","e","f"],
+           4:["g","h","i"],
+           5:["j","k","l"],
+           6:["m","n","o"],
+           7:["p","q","r","s"],
+           8:["t","u","v"],
+           9:["w","x","y","z"]}
 chiffres = ["A","B","C","D","E","F","G","H","I","J"]
+numbers = ["a","b","c","d","e","f","g","h","i","j"]
+
+def GenerateCode (i,kb,nb):
+    coded = ""
+    if i == " ":
+        coded += "00"
+    for k in range(0,len(kb)-1,1):
+        indice = k + 2
+        if i != " ":
+            if i in kb[indice]:
+                index=kb[indice].index(i)
+                if index != (index+1):
+                    coded+=((index+1) * str(indice))
+                else:
+                    coded+=((index+1) * str(indice)) + str(0)
+            elif i in ["'",'.',',',':','?','!','’']:
+                coded+=i
+                break
+            elif i >= '0' and i <= '9':
+                coded+=nb[int(i)]
+                break
+    return coded
 
 def CoderPhrase(phrase):
     coded = ""
     for i in phrase:
-        if i == " ":
-            coded += "00"
-        for k in range(0,len(clavier)-1,1):
-            indice = k + 2
-            if i != " ":
-                if i in clavier[indice]:
-                    index=clavier[indice].index(i)
-                    
-                    if index != (index+1):
-                        coded+=((index+1) * str(indice))
-                    else:
-                        coded+=((index+1) * str(indice)) + str(0)
-                elif i in ["'",'.',',',':','?','!']:
-                    coded+=i
-                    break
-                elif i >= '0' and i <= '9':
-                    coded+=chiffres[int(i)]
-                    break
-                else:
-                    ""
-                    
-                     
+        if (i>="A" and i <= "Z"):
+            coded += GenerateCode(i,clavier,chiffres)
+        elif i>="a" and i<= "z":
+            coded += GenerateCode(i,keybord,numbers)
+        elif i >= '0' and i <='9' or i in ["'",'.',',',':','?','!','’',' ']:
+            coded += GenerateCode(i,keybord,numbers)
     return coded
-
 # - J’ai 17,5 en algo ⇔ 5’244400bh,f0033660025554666
-text ="J'AI 17,5 EN ALGO"
+text ="J’ai 17,5 en algo"
 print(CoderPhrase(text))
---
+++ README.md
@@ -0,0 +1,18 @@
+# Écrire un programme qui, à partir d’une phrase, affiche son équivalent codé
+# Règles de codage de la phrase :
+# On considère le clavier avec les anciens téléphones avec touches.
+# - Pour Écrire la lettre A : il faut appuyer une fois sur la touche 2
+# - Pour Écrire la lettre B : il faut appuyer deux fois sur la touche 2
+# - Pour écrire la lettre S : il faut appuyer trois fois sur la touche 7.
+# Dans le programme on a les règles suivantes :
+# - Pour écrire des chiffres, on écrit des lettres et les lettres deviennent des chiffres :
+# - Pour écrire le chiffre ZÉRO (0), on écrit : A,
+# - Le chiffre UN (1) devient la lettre B, ainsi de suite jusqu’au chiffre NEUF (9) qui
+# devient la lettre J.
+# - Le caractère ESPACE est représenté par DEUX ZÉROS (00).
+# - Les autres caractères ne sont pas changés.
+# - Si des lettres partageant le même chiffre se succèdent, alors il faut les séparer par le
+# chiffre ZÉRO (0)
+# - Exemples :
+# - Bonjour aly ⇔ 22666066566688777002555999
+# - J’ai 17,5 en algo ⇔ 5’244400bh,f0033660025554666
