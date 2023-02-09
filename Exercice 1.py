9a89587347c047e67ca011be40dddbab498127a3
Mahmoud THIAM <dooro.mah@gmail.com>
2023-02-09 19:41:12 +0000

    Mis à jour de l'exercice uno

--- Exercice 1.py
+++ Exercice 1.py
@@ -1,28 +1,50 @@
+from os import system
 fr = ["Janvier","Février","Mars", "Avril","Mai","Juin","Juillet","Août","Septembre","Octobre","Novembre","Décembre"]
 eng = ["January","February","March", "April","May","June","July","Agust","September","October","November","December"]
 mois=[]
 def afficherMois(List):
-        mois.append(List[0:12:3])
-        mois.append(List[1:12:3])
-        mois.append(List[2:12:3])
+        mois = [OneLine(List,0),OneLine(List,1),OneLine(List,2)]
+        print("\n")
+        print("\n")
         for row in mois:
-            print('| {:15} | {:15} | {:15} | {:15} |'.format(*row))
+            print("\t\t\t\t-------------------------------------------------------------------------")
+            print('\t\t\t\t| {:15} | {:15} | {:15} | {:15} |'.format(*row))
+            
+        print("\n")
         
 def saisie():
-    c = input()
+    Affichage()
+    c = input("\t\t\t\t\t\t\tFaites votre choix: ")
     if c == '1':
+        system('clear')
         afficherMois(fr)
         saisie()
     elif c == '2':
+        system('clear')
         afficherMois(eng)
         saisie()
     elif c == '3':
         exit
     else:
         saisie()
-print("1-Mois en français")
-print("2-Mois en anglais")
-print("3-Quitter")
-print("Choix:")
+        
+def Affichage():
+    print("\t\t\t\t\t\t\t---------------------------------")
+    print("\t\t\t\t\t\t\t|\t3-Mois en français\t|")
+    print("\t\t\t\t\t\t\t|\t2-Mois en anglais\t|")
+    print("\t\t\t\t\t\t\t|\t3-Quitter\t\t|")
+    print("\t\t\t\t\t\t\t---------------------------------")
+    
+def OneLine(list,start):
+    newList = []
+    for i in range(start,CustomLength(list),3):
+        newList.append(list[i])
+    return newList
+
+def CustomLength(list):
+    k = 0
+    for i in list:
+        k+=1
+    return k
 afficherMois(fr)
 saisie()
