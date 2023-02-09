deec84f7fed980d0155c0ec6d9bb86b395f2600e
Mahmoud THIAM <dooro.mah@gmail.com>
2023-02-09 12:29:20 +0000

    Intégration des fichiers

--- fn/donnees.py
+++ fn/donnees.py
@@ -47,6 +47,27 @@ def Entete():
         print('|\tPrénom\t|\tNom\t|\tTéléphone\t|\tClasses\t|\tDev\t|\tProj\t|\tExam\t|\tMoyenne\t|')
         print('+---------------------------------------------------------------------------------------------------------------------------------------+')
 
+def CustomSplit(ch,sep):
+    if ch[-1]==sep:
+        pass
+    else:
+        ch+=sep
+    ligne=""
+    std = []
+    for i in ch:
+        if i != sep:
+            ligne+=i
+        else:
+            std.append(ligne)
+            ligne=""
+            continue
+    return std
+
+def CustomLength(list):
+    k = 0
+    for i in list:
+        k+=1
+    return k
 
 def CustomAppend(list,el):
     el = [el]
@@ -55,14 +76,14 @@ def CustomAppend(list,el):
 
 def CustomSort(e,sens):
     if sens=="asc":
-        for i in range(0,len(e)):
-            for j in range(i+1,len(e)):
+        for i in range(0,CustomLength(e)):
+            for j in range(i+1,CustomLength(e)):
                 if e[i]["moy"] > e[i+1]["moy"]:
                     e[i],e[j]=e[j],e[i]
         return e
     else:
-        for i in range(0,len(e)):
-            for j in range(i+1,len(e)):
+        for i in range(0,CustomLength(e)):
+            for j in range(i+1,CustomLength(e)):
                 if e[i]["moy"] < e[i+1]["moy"]:
                     e[i],e[j]=e[j],e[i]
         return e
@@ -81,7 +102,71 @@ def ShowSingleStudent(etudiant):
 
 
 def MenuGenerate(list):
-    for i in range(len(list)):
+    for i in range(CustomLength(list)):
         print('\t\t\t\t+-----------------------------------------------+')
         print('\t\t\t\t|       {} - {}\t\t\t+'.format(i+1,list[i]))
-        print('\t\t\t\t+-----------------------------------------------+')
+        if i == CustomLength(list)-1:
+         print('\t\t\t\t+-----------------------------------------------+')
+
+
+#Supprimer les èspaces entre les chiffres
+def DelSpace(ch):
+    num_tmp=""
+    for k in ch:
+        if k != " ":
+            num_tmp+=k
+    return num_tmp
+
+def IsNumber(ch):
+    num = DelSpace(ch)
+    for c in num:
+        trouve=True
+        if c <'0' or c > '9':
+            trouve = False
+            return trouve
+        else:
+            trouve=True
+    return trouve
+
+#Vérifier la longueur du numéro
+def IsCorrect(ch):
+    num = DelSpace(ch)
+    if IsNumber(num):
+        if CustomLength(num) == 9:
+            if num[0]+num[1] in ['77','78','76','70','75']:
+                return num
+            else:
+                return False
+        else:
+            return False
+    else:
+        return False
+    
+def StudentsToFile(list):
+    f = open("students.txt", "w")
+    for k in list:
+        f.write("{}-{}-{}-{}-{}-{}-{}#".format(k["nom"],k["prenom"],k["telephone"],k["classe"],k["dev"],k["projet"],k["exam"])) 
+    f.close()
+    
+def FileToStudent():
+    f = open("students.txt", "r")
+    ch = f.read()
+
+    std = CustomSplit(ch,"#")
+    lstd = []
+
+    for j in std:
+        et = CustomSplit(j,"-")
+        lstd.append({
+            "nom": et[0],
+            "prenom": et[1],
+            "telephone": int(et[2]),
+            "classe": et[3],
+            "dev": float(et[4]),
+            "projet": float(et[5]),
+            "exam": float(et[6]),
+            "moy": 0.0
+        })
+    return lstd
+
+
--- fn/functions.py
+++ fn/functions.py
@@ -1,6 +1,11 @@
 from os import system
-from fn.donnees import Entete,CustomAppend,ShowSingleStudent,MenuGenerate,CustomSort
-def Menu(e):
+from os.path import exists
+from fn.donnees import Entete,CustomAppend,ShowSingleStudent,MenuGenerate,CustomSort,CustomLength,IsCorrect,StudentsToFile,FileToStudent
+def Menu(etudiants):
+    if exists("students.txt"):
+        e = FileToStudent()
+    else:
+        e = etudiants
     MenuGenerate(["\t AFFICHER TOUT","\t TRIER\t","\t RECHERCHE","\t MODIFICATION","\t NOUVEAU","\t SORTIR "])
     choice = input("\t\t\t\tEnter votre choix: ")
     if choice == "1":
@@ -17,6 +22,7 @@ def Menu(e):
     elif choice == "5":
         ModifierEtudiant(SaisiEtudiant(e))
     elif choice =="6":
+        StudentsToFile(e)
         exit    
 
 def afficherTout(etudiants):
@@ -28,15 +34,15 @@ def afficherTout(etudiants):
 def TAMenu(e):
     system("clear")
     afficherTout(e)
-    MenuGenerate(["CROISSANT","DECROISSANT","RETOUR","QUITTER"])
+    MenuGenerate(["\tCROISSANT","\tDECROISSANT","\tRETOUR\t","\tQUITTER\t"])
     choice = input("\t\t\t\tEnter votre choix: ")
     if choice == "2":
-        for i in range(len(e)):
+        for i in range(CustomLength(e)):
             newEtudiant = CustomSort(e,"desc")
         afficherTout(newEtudiant)
         TAMenu(newEtudiant)
     elif choice == "1":
-        for i in range(len(e)):
+        for i in range(CustomLength(e)):
             newEtudiant =CustomSort(e,"asc")
         afficherTout(newEtudiant)
         TAMenu(newEtudiant)
@@ -51,7 +57,7 @@ def TAMenu(e):
 
 def RechercheEtudiant(e):
     etudiant= {}
-    MenuGenerate(["PAR NOM","PAR PRENOM","PAR TELEPHONE","PAR CLASSE"])
+    MenuGenerate(["\tPAR NOM\t","\tPAR PRENOM","\tPAR TELEPHONE","\tPAR CLASSE"])
     choix = input("\t\t\t\tChoisir le critère de recherche: ")
     if choix == "1":
         nom = input("\t\t\t\tEntrer le nom de l'étudiant: ")
@@ -61,7 +67,13 @@ def RechercheEtudiant(e):
         etudiant = RecherchePar(e,"prenom",prenom)
     elif choix == "3":
         telephone = input("\t\t\t\tEntrer le numéro de téléphone de l'étudiant: ")
-        etudiant = RecherchePar(e,"telephone",int(telephone))
+        telephone = IsCorrect(telephone)
+        if not telephone:
+            system('clear')
+            print("Numéro incorrect")
+            RechercheEtudiant(e)
+        else:
+            etudiant = RecherchePar(e,"telephone",int(telephone))
     elif choix == "4":
         classe = input("\t\t\t\tEntrer la classe de l'étudiant: ")
         etudiant = RecherchePar(e,"classe",classe)
@@ -94,36 +106,45 @@ def ModifierEtudiant(e):
     afficherTout(e)
     NewStudentList= []
     num = input("\t\t\t\tEntrer le numéro de téléphone de l'étudiant à modifier: ")
-    MenuGenerate(["Dev  ","Proj ","Exam "])
-    choix = input("Choisir la note à modifier: ")
-    if choix == "1":
-        note = float(input("\t\t\t\tEntrer la nouvelle note Dev: "))
-        NewStudentList = Modifier(e,num,"dev",note)
-    elif choix == "2":
-        note = float(input("\t\t\t\tEntrer la nouvelle note Proj: "))
-        NewStudentList = Modifier(e,num,"projet",note)
-    elif choix == "3":
-        note = float(input("\t\t\t\tEntrer la nouvelle note Exam: "))
-        NewStudentList = Modifier(e,num,"exam",note)
+    num = IsCorrect(num)
+    if not num:
+            ModifierEtudiant(e)
     else:
-        system("clear")
-        print("Veuillez faire un choix correct.")
-        ModifierEtudiant(e)
-    if choix in ["1","2","3"]:
-        afficherTout(NewStudentList)
-        Menu(NewStudentList)
+        MenuGenerate(["\t\tDev","\t\tProj","\t\tExam"])
+        choix = input("Choisir la note à modifier: ")
+        if choix == "1":
+            note = float(input("\t\t\t\tEntrer la nouvelle note Dev: "))
+            NewStudentList = Modifier(e,num,"dev",note)
+        elif choix == "2":
+            note = float(input("\t\t\t\tEntrer la nouvelle note Proj: "))
+            NewStudentList = Modifier(e,num,"projet",note)
+        elif choix == "3":
+            note = float(input("\t\t\t\tEntrer la nouvelle note Exam: "))
+            NewStudentList = Modifier(e,num,"exam",note)
+        else:
+            system("clear")
+            print("Veuillez faire un choix correct.")
+            ModifierEtudiant(e)
+        if choix in ["1","2","3"]:
+            afficherTout(NewStudentList)
+            Menu(NewStudentList)
         
 # ----------------- SAISI -------------------------
 
 def SaisiEtudiant(e):
     nom = input("\t\t\t\tEntrer le nom de l'étudiant: ")
     prenom = input("\t\t\t\tEntrer le prénom de l'étudiant: ")
-    telephone = int(input("\t\t\t\tEntrer le numéro de téléphone de l'étudiant: "))
+    telephone = input("\t\t\t\tEntrer le numéro de téléphone de l'étudiant: ")
+    telephone = IsCorrect(telephone)
+    if not telephone:
+            system("clear")
+            print("Veuillez saisir un numéro correct")
+            SaisiEtudiant(e)
     classe = input("\t\t\t\tEntrer la classe de l'étudiant: ")
     return CustomAppend(e,{
         "nom": nom,
         "prenom": prenom,
-        "telephone": telephone,
+        "telephone": int(telephone),
         "classe": classe,
         "dev": 0.0,
         "projet": 0.0,
