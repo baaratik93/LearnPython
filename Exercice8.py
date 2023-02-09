9504a5ecac9fccfb60528793a904d504eb7a25ca
Mahmoud THIAM <dooro.mah@gmail.com>
2023-02-09 17:13:10 +0000

    Mise à jour del'exercice

--- Exercice8.py
+++ Exercice8.py
@@ -1,13 +1,3 @@
 from fn.functions import Menu
 from fn.donnees import students
-# from fn.donnees import Entete
-# Saiisie des informations l'étudiant
-# Controler la saisie d'informations
-# Gérer les fonctions externes
-# Gêrer le menu
-# Trie et affichage
-# Recherche selon le critère
-# Modification de note pour un étudiant
-# Sortir de l'application
-# Gestion de la navigation
 Menu(students())
